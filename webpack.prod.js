4bb80a2b8aa452344e36e88755dc1abed9909529
PhucNguyen <nguyen.hphuc035@gmail.com>
2018-06-18 02:22:24 +0700

    Add minify css in webpack production config. optimize-css-assets-webpack-plugin buggy, cant transform source maps

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,30 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: {
+          preset: 'default',
+          map: { inline: false }
+        }
+      })
+    ]
+  },
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
+    // new UglifyJSPlugin({ sourceMap: true }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
