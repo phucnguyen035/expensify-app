9cb4507fbfbb75e86a8f2aa967bd6ec430951c70
PhucNguyen <nguyen.hphuc035@gmail.com>
2018-06-17 23:07:02 +0700

    Splitting webpack

---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/app.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/scripts/',
+    historyApiFallback: true
+  },
+
+  devtool: 'cheap-module-eval-source-map'
+});
